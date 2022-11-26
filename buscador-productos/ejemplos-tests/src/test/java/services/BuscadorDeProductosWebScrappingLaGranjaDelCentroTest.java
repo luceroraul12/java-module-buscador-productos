54827d9052cf7bf0b2e6c3701845d8508ad19f72
Lucero Raul <luceroraul12@gmail.com>
2022-11-26 08:01:43 -0300

    funcionamiento probado con almenos una distribuidora en este caso la granja del centro

--- buscador-productos/ejemplos-tests/src/test/java/services/BuscadorDeProductosWebScrappingLaGranjaDelCentroTest.java
+++ buscador-productos/ejemplos-tests/src/test/java/services/BuscadorDeProductosWebScrappingLaGranjaDelCentroTest.java
@@ -4,10 +4,17 @@ import entity.LaGranjaDelCentro;
 import org.jsoup.Jsoup;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockedConstruction;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
@@ -16,17 +23,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BuscadorDeProductosWebScrappingLaGranjaDelCentroTest {
 
-    @Mock
-    static BuscadorDeProductosWebScrappingLaGranjaDelCentro servicio = Mockito.mock(BuscadorDeProductosWebScrappingLaGranjaDelCentro.class);
+    BuscadorDeProductosWebScrappingLaGranjaDelCentro servicio;
 
-    @BeforeAll
-    static void beforeAll() throws IOException {
-        Mockito.when(servicio.generarDocumentos()).thenReturn(Arrays.asList(Jsoup.connect("https://lagranjadelcentro.com.ar/productos.php?pagina=3").get()));
+    @BeforeEach
+    void beforeAll() throws IOException {
+        servicio = Mockito.spy(new BuscadorDeProductosWebScrappingLaGranjaDelCentro());
+        Mockito
+                .doReturn(Arrays.asList(Jsoup.parse(new File("src/main/resources/la-granja-del-centro.html"))))
+                .when(servicio).generarDocumentos();
     }
 
     @Test
-    void obtenerProductosAPartirDeElements() {
+    void obtenerProductosAPartirDeElements() throws IOException {
+
         List<LaGranjaDelCentro> productos = servicio.adquirirProductosEntidad();
-        Assertions.assertTrue(10 < productos.size());
+        Assertions.assertEquals(4 , productos.size());
     }
 }
